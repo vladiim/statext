b0861721f77e30c345f5df779918324c487c9f4a
Vlad Mehakovic <vlad.mehakovic@X-Vlad-Mehakovic-MBP13.local>
2016-03-28 13:54:54 +1100

    Revised dashboard render.js

--- public/javascripts/lib/render_reports.js
+++ public/javascripts/lib/render_reports.js
@@ -18,11 +18,6 @@
       });
       this.deleteLoader();
     },
-    // reportData: function() {
-    //   $.get('/reports', function(data) {
-    //     return data;
-    //   });
-    // },
     deleteLoader: function() {
       $(LOADING_REPORTS).remove();
     },
@@ -51,43 +46,3 @@
     RenderReports.init();
   })
 })();
-
-// (function() {
-//   var GA_PROFILE_MANAGER = '#google-analytics-profile-manager';
-//   var SelectForm = {
-//     init: function() {
-//       if (this.hasGAProfileManager()) {
-//         this.buildForm();
-//       }
-//     },
-//     hasGAProfileManager: function() {
-//       return $(GA_PROFILE_MANAGER).length > 0;
-//     },
-//     buildForm: function() {
-//       R.map(this.addOption, this.getGAProfiles());
-//     },
-//     getGAProfiles: function() {
-//       return [{
-//         name: 'PROFILE NAME',
-//         id: 'PROFILE ID',
-//         web_property: 'WEB PROPERTY',
-//         account: 'ACCOUNT NAME'
-//       }]
-//     },
-//     addOption: function(profile) {
-//       $(GA_PROFILE_MANAGER)
-//         .find('select.form-control')
-//         .append(
-//           '<option data-id="' +
-//           profile.id + '">' +
-//           profile.name + ' > ' +
-//           profile.web_property + ' > ' +
-//           profile.account +
-//           '</option>'
-//         )
-//     }
-//   }
-//   $(document).ready(function() {
-//     SelectForm.init();
-//   });
-// })();
